b01de085289b8e918fec27a03773175d8088126d
Nikita <nteplitskiy@TX150>
2020-09-09 14:08:39 -0400

    works better now

--- a1.py
+++ a1.py
@@ -7,7 +7,7 @@ N = 50
 x = tf.random.uniform([N], dtype=tf.float64)
 #x = np.random.rand(N)
 #print(x)
-y = tf.math.sin(np.pi * 2 * x) #+ 0.1 * np.random.randn(N)
+y = tf.math.sin(np.pi * 2 * x) + 0.1 * np.random.randn(N)
 #y = np.sin(np.pi * 2 * x)
 #print(y)
 
@@ -22,15 +22,15 @@ plt.plot(x.numpy(), y.numpy(), '.')
 
 M = 10
 
-b = tf.random.uniform([M, 1])
-u = tf.random.uniform([M, 1])
-q = tf.random.uniform([M, 1])
-w = tf.random.uniform([M, 1])
+#b = tf.random.uniform([M, 1])
+#u = tf.random.uniform([M, 1])
+#q = tf.random.uniform([M, 1])
+#w = tf.random.uniform([M, 1])
 
 my_params = {
     'w' : tf.Variable(np.zeros([M,1]) + 0.1),
     'b' : tf.Variable(np.zeros([M,1]) + 0.0),
-    #'b' : tf.Variable(0.0, dtype=tf.float64),
+    #'b' : tf.Variable(np.array([0.0]).reshape(-1,1), dtype=tf.float64),
     'mu' : tf.Variable(np.random.uniform(0,1,M).reshape(-1,1)),
     'sig': tf.Variable(np.zeros([M,1]) + 0.1)
 }
@@ -51,16 +51,16 @@ plt.plot(sin_x, ewave)
 plt.show()
 
 rate = 0.01
-num_iter = 20
+num_iter = 10
 
 for i in range(num_iter):
     with tf.GradientTape() as tape:
         loss = j(my_params, x, y)
     grads = tape.gradient(loss, my_params)
     print(i)
-    print(grads)
+    print(grads['b'])
     my_params['w'].assign(my_params['w'].numpy() - rate*grads['w'].numpy())
-    #my_params['b'].assign(my_params['b'].numpy() - rate*grads['b'].numpy())
+    #my_params['b'].assign(my_params['b'].numpy() - 0.0001*grads['b'].numpy())
     my_params['mu'].assign(my_params['mu'].numpy() - rate*grads['mu'].numpy())
     my_params['sig'].assign(my_params['sig'].numpy() - rate*grads['sig'].numpy())
     #ewave = y_h(my_params, sin_x)
@@ -73,42 +73,17 @@ print(my_params)
 ewave = y_h(my_params, sin_x)
 plt.plot(sin_x, sin_y)
 plt.plot(sin_x, ewave)
+plt.plot(x.numpy(), y.numpy(), '.')
 plt.show()
 
-#step_count = opt.minimize(j, x)
+for base in range(M):
+    sparm = {}
+    for x in my_params:
+        #print("HELLO")
+        #print(base, x)
+        #print(my_params[x][base])
+        sparm[x] = ((my_params[x]).numpy()[base]).reshape(-1,1)
+    print(sparm)
+    plt.plot(sin_x, y_h(sparm, sin_x))
 
-#b = np.empty([N,1]) + 1;
-#q = w = b
-
-#b = -1
-#q = 1
-#w = 1
-#u = 0.5
-
-
-
-#phi = np.exp(-(x-u)**2 / q**2)
-#y_h = w * phi + b
-
-#plt.plot(s_x, y_h(parm,s_x))
-#plt.show()
-
-# 1/2 * (y - y_h)**2
-
-#xx = tf.constant(x)
-#with tf.GradientTape as t:
-    
-
-
-#xx = tf.constant(x)
-#t = tf.GradientTape()
-#t.watch(xx)
-
-#y_hh = y_h(w,x,u,q,b)
-#y_hh = y_h(parm,x)
-#e = j(y, y_hh)
-
-
-
-#plt.plot(x, e, '.')
-#plt.show()
+plt.show()
