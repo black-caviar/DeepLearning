85888fd5144b3f3a1bfa48c0567c6b06b30552d0
Nikita <nteplitskiy@TX150>
2020-09-09 21:44:18 -0400

    images done

--- a1/a1.py
+++ a1/a1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 N = 50 #num datapoints
 M = 10 #num basis functions 
 rate = 0.01 #adjustment rate
-num_iter = 200 #num optimization iterations
+num_iter = 100 #num optimization iterations
 
 #generate noisy sinusoidal data 
 #x = tf.random.uniform([N], dtype=tf.float64)
@@ -22,7 +22,7 @@ params = {
     'b' : tf.Variable(np.zeros([M,1]) + 0.0),
     #'b' : tf.Variable(np.array([0.0]).reshape(-1,1), dtype=tf.float64),
     'mu' : tf.Variable(np.random.uniform(0,1,M).reshape(-1,1)),
-    'sig': tf.Variable(np.zeros([M,1]) + 0.3)
+    'sig': tf.Variable(np.zeros([M,1]) + 0.2)
     #'sig': tf.Variable(np.random.uniform(0,1,M).reshape(-1,1))
 }
     
@@ -34,6 +34,7 @@ def j(param, x, y):
     y_est = y_h(param, x)
     return (y-y_est)**2 / 2
 
+#why am I doing loops instead of using matrices?
 def plot_basis(M, p, x):
     for j in range(M):
         p_j = {}
@@ -44,21 +45,20 @@ def plot_basis(M, p, x):
 #show initial basis functions 
 plot_basis(M, params, clean_x)
 plt.title("Initial Basis Functions")
-plt.xlabel('x')
-plt.ylabel('y', rotation=0)
-plt.xlim([0,1])
+plt.xlabel('x'); plt.ylabel('y', rotation=0); plt.xlim([0,1])
 #plt.show()
 plt.savefig('initial_basis.pdf', format='pdf')
 plt.clf()
 
 #view before optimization
 fit_bad = y_h(params, clean_x)
-plt.plot(clean_x, clean_y)
-plt.plot(clean_x, fit_bad, '--')
-plt.plot(x.numpy(), y.numpy(), '.')
+plt.plot(clean_x, clean_y, label='Clean Sin')
+plt.plot(clean_x, fit_bad, '--', label='Initial Fit')
+plt.plot(x.numpy(), y.numpy(), '.', label='Data points')
 plt.xlabel('x'); plt.ylabel('y', rotation=0); plt.xlim([0,1])
 plt.title("Estimate Before Optimization")
-#plt.show()
+plt.legend()
+plt.show()
 plt.savefig('before_opt.pdf', format='pdf')
 plt.clf()
 
@@ -73,19 +73,16 @@ for i in range(num_iter):
     params['mu'].assign(params['mu'] - rate*grad['mu'])
     params['sig'].assign(params['sig'] - rate*grad['sig'])
 
-#print(params)
 #plot the optimized result
 y_fit = y_h(params, clean_x)
-plt.plot(clean_x, clean_y)
-plt.plot(clean_x, y_fit, '--')
-plt.plot(x.numpy(), y.numpy(), '.')
+plt.plot(clean_x, clean_y, label='Clean Sin')
+plt.plot(clean_x, y_fit, '--', label='Optimized Fit')
+plt.plot(x.numpy(), y.numpy(), '.', label='Data points')
 #plt.plot(clean_x, j(params, clean_x, clean_y))
 plt.title("Estimate After Optimization")
-plt.xlabel('x')
-plt.ylabel('y', rotation=0)
-plt.xlim([0,1])
-#plt.ylim([-1,1])
-#plt.show()
+plt.legend()
+plt.xlabel('x'); plt.ylabel('y', rotation=0); plt.xlim([0,1])
+plt.show()
 plt.savefig('after_opt.pdf', format='pdf')
 plt.clf()
 
