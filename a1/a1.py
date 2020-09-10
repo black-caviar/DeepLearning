a9639a78756cbc3e6f3d5f4fbe7f5f97452c757e
Nikita Teplitskiy <nteplitskiy@localhost.localdomain>
2020-09-09 21:56:07 -0400

    final commit

--- a1/a1.py
+++ a1/a1.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+#tunable parameters
 N = 50 #num datapoints
 M = 10 #num basis functions 
 rate = 0.01 #adjustment rate
@@ -20,7 +21,6 @@ clean_y = np.sin(np.pi * 2 * clean_x)
 params = {
     'w' : tf.Variable(np.zeros([M,1]) + 0.1),
     'b' : tf.Variable(np.zeros([M,1]) + 0.0),
-    #'b' : tf.Variable(np.array([0.0]).reshape(-1,1), dtype=tf.float64),
     'mu' : tf.Variable(np.random.uniform(0,1,M).reshape(-1,1)),
     'sig': tf.Variable(np.zeros([M,1]) + 0.2)
     #'sig': tf.Variable(np.random.uniform(0,1,M).reshape(-1,1))
@@ -34,6 +34,7 @@ def j(param, x, y):
     y_est = y_h(param, x)
     return (y-y_est)**2 / 2
 
+#helper function for plotting basis functions 
 #why am I doing loops instead of using matrices?
 def plot_basis(M, p, x):
     for j in range(M):
@@ -67,9 +68,10 @@ for i in range(num_iter):
     with tf.GradientTape() as tape:
         loss = j(params, x, y)
     grad = tape.gradient(loss, params)
+    #update parameters with gradient scaled by rate
     params['w'].assign(params['w'] - rate*grad['w'])
     params['b'].assign(params['b'] - rate*np.divide(params['b'].numpy(), grad['b'].numpy()))
-    #scale because otherwise result is unreasonable
+    #scale b by itself because otherwise result is unreasonable
     params['mu'].assign(params['mu'] - rate*grad['mu'])
     params['sig'].assign(params['sig'] - rate*grad['sig'])
 
@@ -86,6 +88,7 @@ plt.savefig('after_opt.pdf', format='pdf')
 plt.show()
 plt.clf()
 
+#plot basis functions after optimization
 plot_basis(M, params, clean_x)
 plt.title("Basis Functions After Optimization")
 plt.xlabel('x'); plt.ylabel('y', rotation=0); plt.xlim([0,1])
