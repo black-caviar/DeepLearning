8d8f6e5ff84927e8796a9e2c8b0b56deab68df9b
Nikita <nteplitskiy@TX150>
2020-09-23 20:28:58 -0400

    some architectural changes

--- a3/a3.py
+++ a3/a3.py
@@ -3,51 +3,74 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import Model
 
-from tensorflow.keras.layers import Dense, Flatten, Conv2D
-from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout
+#from tensorflow.keras import Model
 
 import numpy as np
+import matplotlib.pyplot as plt
+import MNISTload
 
 mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+(x_pool, y_pool), (x_test, y_test) = MNISTload.load_MNIST()
+#convert to float
+x_pool, x_test = x_pool.astype("float32") / 255.0, x_test.astype("float32") / 255.0
 
-# Add a channels dimension
-x_train = x_train[..., tf.newaxis].astype("float32")
-x_test = x_test[..., tf.newaxis].astype("float32")
+x_train, y_train = x_pool[:3*len(x_pool)//4], y_pool[:3*len(x_pool)//4]
+x_valid, y_valid = x_pool[3*len(x_pool)//4:], y_pool[3*len(x_pool)//4:]
 
-train_ds = tf.data.Dataset.from_tensor_slices(
-    (x_train, y_train)).shuffle(10000).batch(32)
-
-test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+#try to get rid of these datasets
+#can use Dataset.take Dataset.skip for some of these
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(32)
+valid_ds = tf.data.Dataset.from_tensor_slices((x_valid, y_valid)).batch(32)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(1)
 
 class MyModel(Model):
   def __init__(self):
     super(MyModel, self).__init__()
-    #self.conv1 = Conv2D(32, 3, activation='relu')
-    self.flatten = Flatten()
-    self.d1 = Dense(64, activation='relu')
+    self.flatten = Flatten() #just a convenience layer to reshape my inputs 
+    self.d1 = Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.0001))
+    self.drp1 = Dropout(0.1)
     self.d2 = Dense(10)
-
   def call(self, x):
-    #x = self.conv1(x)
     x = self.flatten(x)
     x = self.d1(x)
+    x = self.drp1(x)
     return self.d2(x)
 
 # Create an instance of the model
 model = MyModel()
 
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
 optimizer = tf.keras.optimizers.Adam()
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
 train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
 
+valid_loss = tf.keras.metrics.Mean(name='valid_loss')
+valid_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='valid_accuracy')
+
 test_loss = tf.keras.metrics.Mean(name='test_loss')
 test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+model.compile(optimizer=optimizer, loss=loss_object, metrics=[train_accuracy])
+history = model.fit(x_train, y_train, batch_size=128, epochs=5, validation_data=(x_valid,y_valid))
+plt.plot(history.history['loss'])
+plt.show()
+exit()
+
+@tf.function
+def step(x,y):
+  with tf.GradientTape() as tape:
+    #print(x,y)
+    y_h = model(x, training=True)
+    #print(y_h)
+    loss = loss_object(y,y_h)
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(y, y_h)
 
 @tf.function
 def train_step(images, labels):
@@ -81,8 +104,17 @@ for epoch in range(EPOCHS):
   test_loss.reset_states()
   test_accuracy.reset_states()
 
+  #for x,y in zip(x_train,y_train):
+  #  print(x.shape)
+  #  print(y)
+  #  print(type(y))
+  #  step(x,y.astype("float32"))
+  
   for images, labels in train_ds:
-    train_step(images, labels)
+    #print(images.shape)
+    #print(labels.shape)
+    #train_step(images, labels)
+    step(images,labels)
 
   for test_images, test_labels in test_ds:
     test_step(test_images, test_labels)
@@ -93,3 +125,6 @@ for epoch in range(EPOCHS):
                         train_accuracy.result() * 100,
                         test_loss.result(),
                         test_accuracy.result() * 100))
+
+#for test_images, test_labels in test_ds:
+#    test_step(test_images, test_labels)
