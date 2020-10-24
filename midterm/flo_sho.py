4e4a38ad5bfad341c225388408a76e2f359ea947
Nikita <nteplitskiy@TX150>
2020-10-24 17:00:37 -0400

    work on image showing

---
+++ midterm/flo_sho.py
@@ -0,0 +1,29 @@
+import glob
+import flowiz as fz
+import matplotlib.pyplot as plt
+import IO
+
+num = 1000
+
+img1 = sorted(glob.glob('FlyingChairs_release/data/*img1.ppm'))
+img2 = sorted(glob.glob('FlyingChairs_release/data/*img2.ppm'))
+flow = sorted(glob.glob('FlyingChairs_release/data/*.flo'))
+
+plt.figure(1)
+plt.subplot(1,3,1)
+plt.imshow(plt.imread(img1[num]))
+plt.subplot(1,3,2)
+plt.imshow(plt.imread(img2[num]))
+
+plt.subplot(1,3,3)
+flow_img = fz.convert_from_file(flow[num])
+plt.imshow(flow_img)
+plt.show()
+
+#flow_img = IO.read(flow[num])
+#plt.imshow(fz.convert_from_flow(flow_img))
+#plt.show()
+#
+#img = plt.imread(img1[num])
+#plt.imshow(img)
+#plt.show()
