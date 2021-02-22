6d0e52cf87ed58296c74eb9459aac6bf9ed05b02
Nikita <nteplitskiy@TX150>
2021-02-21 22:06:34 -0500

    make it work

--- pepeGAN/load_dataset.py
+++ pepeGAN/load_dataset.py
@@ -1,30 +1,23 @@
 import sys
 import PIL
 import tensorflow as tf
-AUTOTUNE = tf.data.experimental.AUTOTUNE
+AUTOTUNE = tf.data.experimental.AUTOTUNE    
 
-def get_binary_dataset(path, batch=8):
-    x = tf.keras.preprocessing.image_dataset_from_directory(
-        path,
-        label_mode='binary',
-        batch_size=batch,
-        class_names=['not_pepe','pepe'],
-    )
-    return mobilenet_preprocess(x)
+def mobilenet_preprocess(x,y):
+    #x = x.cast(x, tf.float32)
+    x /= 127.5
+    x -= 1.
+    return x,y
 
-def mobilenet_preprocess(x):
-    x = x.map(lambda x,y: (x / 127.5, y))
-    x = x.map(lambda x,y: (x - 1., y))
-    return x
+def load_256(x,y):
+    x = tf.io.read_file(x)
+    x = tf.io.decode_image(x, expand_animations=False)
+    x = tf.image.resize(x, [256,256])
+    return x,y 
 
-
-def get_dataset(path):
-    x = load_directory(path)
-    x = x.map(lambda x: x / 127.5)
-    x = x.map(lambda x: x - 1.)
-    return x
+def get_image_list(path):
+    return tf.data.Dataset.list_files(path + '*', shuffle=False)
     
-
 def gen_dataset(path):
     list_ds = tf.data.Dataset.list_files(path + '*.*')
     data = list_ds.map(tf.io.read_file)
@@ -40,18 +33,42 @@ def load_directory(path):
         #label_mode=None,
         );
 
+def construct_datasets(p, np):
+    n_p = tf.data.experimental.cardinality(p).numpy()
+    n_np = tf.data.experimental.cardinality(np).numpy()
+    print('Pepe images:', n_p)
+    print('Non-Pepe images:', n_np)
+    
+    p = p.map(lambda x: (x,1))
+    np = np.map(lambda x: (x,0))
+    p = p.shuffle(3000)
+    np = np.shuffle(3000)
+    
+    pnp = tf.data.experimental.sample_from_datasets([p, np])
+    n_pnp = n_p + n_np
+    pnp_train = pnp.take(int(0.8*n_pnp))
+    pnp_val = pnp.skip(int(0.8*n_pnp))
+    
+    pnp_train = pnp_train.map(load_256)
+    pnp_val = pnp_val.map(load_256)
+    # I don't think this matters 
+    return pnp_train, pnp_val 
+
 if __name__ == "__main__":
     import os
     os.environ["CUDA_VISIBLE_DEVICES"]="-1" 
     import matplotlib.pyplot as plt
-    data = load_directory(sys.argv[1])
-    x = get_binary_dataset(sys.argv[1])
+    #data = load_directory(sys.argv[1])
+    p = get_image_list(sys.argv[1])
+    np = get_image_list(sys.argv[2])
+    x,y = construct_datasets(p,np)
+    #x = get_binary_dataset(sys.argv[1])
     i=0
-    for f in x.unbatch().take(9):
+    for f in y.take(9):
         ax = plt.subplot(3, 3, i + 1)
-        #plt.imshow(f.numpy().astype("uint8"))
-        plt.imshow((f[0].numpy()+1)/2)
+        plt.imshow(f[0].numpy().astype("uint8"))
+        #plt.imshow(f[0].numpy())
         print(f[1].numpy())
-        plt.title(f[1].numpy())
+        plt.title(int(f[1].numpy()))
         i=i+1
     plt.show()
--- pepeGAN/train_ispepe.py
+++ pepeGAN/train_ispepe.py
@@ -1,10 +1,41 @@
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers 
 import load_dataset as ld
-import argparse 
+import argparse
+import datetime 
 #load complete model
 #save weights
 #save complete model
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE    
+
+data_augmentation = keras.Sequential([
+    layers.experimental.preprocessing.RandomFlip("horizontal"),
+    layers.experimental.preprocessing.RandomRotation(0.2),
+    layers.experimental.preprocessing.RandomCrop(224,224),
+])
+
+def augment_data(x,y):
+    #x = tf.image.grayscale_to_rgb(x)
+    if tf.shape(x)[2] != 3:
+        print("Fuck you 1")
+        x = tf.zeros([224,224,3])
+        return x,y
+        
+    x = tf.image.random_crop(x, [224,224,3]) #random crop to 224
+    x = tf.image.random_flip_left_right(x) #flip half of images
+    # there is no easy rotation in tf.image
+    # but it can be done using PIL 
+    return x,y
+
+def resize_image(x,y):
+    if tf.shape(x)[2] != 3:
+        print("Fuck you 2")
+        x = tf.zeros([224,224,3])
+        return x,y
+    x = tf.image.resize(x, [224,224])
+    return x,y
 
 def main():
     try: model = keras.models.load_model(FLAGS.model)
@@ -12,25 +43,67 @@ def main():
         print(e)
         exit(-1)
 
-    try: pepe = ld.get_dataset(FLAGS.pepe)
+    if FLAGS.checkpoint:
+        model.load_weights(FLAGS.checkpoint)
+
+    try: pepe = ld.get_image_list(FLAGS.pepe)
     except Exception as e:
         print(e)
         exit(-1)
 
-    try: not_pepe = ld.get_dataset(FLAGS.not_pepe)
+    try: not_pepe = ld.get_image_list(FLAGS.not_pepe)
     except Exception as e:
         print(e)
         exit(-1)
-    print("Done loading")
+
+        
+    print("Found image files")
+    # perhaps the validation should occurr before shuffling?
+    # I think so
+
+    # I will have image augmentation of the validation set if I don't split it off now
+    
+    pnp_train, pnp_val = ld.construct_datasets(pepe, not_pepe)
+    pnp_train = pnp_train.map(ld.mobilenet_preprocess)
+    pnp_val = pnp_val.map(ld.mobilenet_preprocess)
+    
+    pnp_train = pnp_train.map(augment_data, num_parallel_calls=AUTOTUNE)
+    pnp_val = pnp_val.map(resize_image)
+    #pnp_train = pnp_train.cache()
+    #preprocessing needs to be done every epoch so do it later?
+
+    pnp_train = pnp_train.batch(32).prefetch(10)
+    pnp_val = pnp_val.batch(32)
+    # my pipeline is bonkers inefficient. Really need to try the layers api
+    
+    pnp_train.cache()
+    pnp_val.cache()
+    
     model.summary()
-
-    pepe.map(lambda x: (x,1))
-    not_pepe.map(lambda x: (x,0))
     
     opt = keras.optimizers.Adam(learning_rate=0.001)
-    model.compile(optimizer=opt, loss='binary_crossentropy')
+    loss = keras.losses.BinaryCrossentropy()
+
+    c_path = 'checkpoints/cp-{epoch:04d}.ckpt'
+    cp_callback = keras.callbacks.ModelCheckpoint(c_path, save_weights_only=True)
+
+    log_dir = 'logdir/fit/' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, profile_batch='2,5')
+
+    #callbacks=[tensorboard_callback, cp_callback]
+    callbacks=[tensorboard_callback]
     
-    #model.fit(
+    model.compile(optimizer=opt, loss=loss, metrics='binary_accuracy')
+    model.fit(
+        pnp_train,
+        epochs=10,
+        validation_data=pnp_val,
+        callbacks=callbacks,
+        use_multiprocessing=True,
+        workers=4,
+    )
+    #model.fit(pnp_train.batch(32), epochs=10,
+    #          validation_data=pnp_train.batch(4), callbacks=callbacks)
         
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
@@ -39,6 +112,12 @@ if __name__ == '__main__':
         type=str,
         required=True,
         help='Path to model'
+    )
+    p.add_argument(
+        '--checkpoint',
+        type=str,
+        required=False,
+        help='Path to last model checkpoint'
     )
     p.add_argument(
         '--pepe',
